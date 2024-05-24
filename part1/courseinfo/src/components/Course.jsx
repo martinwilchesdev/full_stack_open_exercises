1cd1f27537a5e8b176cd505c2c5f60b439dab879
Martin Wilches <martindotdev@gmail.com>
2024-05-24 14:45:36 -0500

    Formateo Prettier courses

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -1,21 +1,19 @@
-import Total from './Total'
-import Header from './Header'
-import Content from './Content'
+import Total from './Total';
+import Header from './Header';
+import Content from './Content';
 
 const Course = ({ courses }) => {
-  return (
-    <>
-      {
-        courses.map(course => (
-          <div key={course.id}>
-            <Header course={course} key={course.id} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-          </div>
-        ))
-      }
-    </>
-  )
-}
+    return (
+        <>
+            {courses.map((course) => (
+                <div key={course.id}>
+                    <Header course={course} key={course.id} />
+                    <Content parts={course.parts} />
+                    <Total parts={course.parts} />
+                </div>
+            ))}
+        </>
+    );
+};
 
-export default Course
+export default Course;
