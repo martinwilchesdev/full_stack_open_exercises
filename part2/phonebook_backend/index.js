8ad5b5238878c7566356789127b2d568a67f23fb
Martin Wilches <martindotdev@gmail.com>
2024-06-11 21:56:37 -0500

    Servidor NodeJs agenda telefonica

---
+++ part2/phonebook_backend/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+// obtener todos los recursos
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date}</p>
+    `)
+})
+
+// obtener un recurso individual
+app.get('/api/persons/:id', (req, res) => {
+    const request_id = Number(req.params.id)
+    const person = persons.find(person => person.id === request_id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const request_id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== request_id)
+
+    res.status(204).end()
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
