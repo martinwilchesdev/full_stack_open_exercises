96a97af217f9b202444914dc04b95b50bc30cf31
Martin Wilches <martindotdev@gmail.com>
2024-06-18 20:50:59 -0500

    Se elimina el llamado del middleware morgan

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -30,12 +30,6 @@ let persons = [
     }
 ]
 
-app.use(express.json())
-
-// logger morgan personalizado
-morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
 // obtener todos los recursos
 app.get('/api/persons', (req, res) => {
     res.json(persons)
